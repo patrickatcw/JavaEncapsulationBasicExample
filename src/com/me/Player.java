821733321c04a855973a125cf03d2480cef725ef
patrickatcw <pj8392@yahoo.com>
2018-03-01 08:11:37 -0500

    class player is an example without encapsulation, created public fields, not private, method in step 3, method in step 4, next go to main step 5 and create instances

---
+++ src/com/me/Player.java
@@ -0,0 +1,35 @@
+package com.me;
+//step 1
+//this is an example class without encapsulation
+//and note no constructor created, but there is a
+// default constructor with no parameters
+
+public class Player {
+
+    //step 2 make fields, made public instead of private
+    //public String name;
+    public String fullName;
+
+    //step 9 changing name to fullName above, error in main
+    //have to fix in step 10
+    public int health;
+    public String weapon;
+
+    //step 3 method losehealth
+    public void loseHealth(int damage){
+        this.health = this.health - damage;
+        if(this.health <= 0) {
+            System.out.println("Player knocked out");
+            //reduce number of lives remaining for player
+        }
+    }
+
+    //step 4 method for health remaining
+    public int healthRemaining(){
+        return this.health;
+
+    }
+
+}
+
+//step 5 go to main to create instances and run program
